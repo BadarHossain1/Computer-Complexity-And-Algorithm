46affe01cec06012b44535a05f1aa6f54f8cf275
Badar Hossain <badar12041@gmail.com>
2024-05-27 12:04:53 +0600

    Dijsktra code pushed

--- DynamicProgramming.cpp/Minimum_Edit_Distance.cpp
+++ DynamicProgramming.cpp/Minimum_Edit_Distance.cpp
@@ -19,11 +19,11 @@ int Min_Edit_Distance(string s1 ,string s2 , int n , int m)
             EDIT_DISTANCE[i][0]= i;
 
 
-            if (i > 0 && j> 0)
+            if (i > 0 && j> 0) //we are checking in this condition if the strings are not empty.
             {
-                if (s1[i-1]== s2[j-1])
+                if (s1[i-1]== s2[j-1]) //i-1 because we are starting from 1 and the string starts from 0.
                 {
-                    EDIT_DISTANCE[i][j] = EDIT_DISTANCE[i-1][j-1];
+                    EDIT_DISTANCE[i][j] = EDIT_DISTANCE[i-1][j-1]; 
                 }
                 else
                 {
@@ -50,25 +50,25 @@ int Min_Edit_Distance(string s1 ,string s2 , int n , int m)
     int i = n;
     int j = m;
 
-    while(EDIT_DISTANCE[i][j] > 0)
+    while(EDIT_DISTANCE[i][j] > 0) //while condition says that we will keep on moving until we reach the 0th row or column.
     {
-        if(s1[i-1] == s2[j-1])
+        if(s1[i-1] == s2[j-1]) //if the characters are same then we will move diagonally
         {
             i--;
             j--;
         }
         else
         {
-            if(  EDIT_DISTANCE[i-1][j] <= EDIT_DISTANCE[i-1][j-1]  &&  EDIT_DISTANCE[i-1][j] <= EDIT_DISTANCE[i][j-1] )
+            if(  EDIT_DISTANCE[i-1][j] <= EDIT_DISTANCE[i-1][j-1]  &&  EDIT_DISTANCE[i-1][j] <= EDIT_DISTANCE[i][j-1] ) //if the characters are not same then we will move to the minimum of the three. the way we get minimum is by comparing the three values and moving to the minimum value.
             {
                 cout<< s1[i-1] << " removed from string 1"<<endl ;
-                i--;
+                i--; //the string which gets removed is the string which is not present in the other string. So we will move to the next character of the string which is present in the other string.
             }
             else if(EDIT_DISTANCE[i-1][j-1] <= EDIT_DISTANCE[i-1][j]  &&  EDIT_DISTANCE[i-1][j-1] <= EDIT_DISTANCE[i][j-1])
             {
                 cout<<"string 1's "<< s1[i-1] << " is replaced with "<< s2[j-1]<<endl;
                 i--;
-                j--;
+                j--;   
             }
             else
             {
